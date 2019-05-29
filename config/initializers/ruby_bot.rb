ff7cefb857cb46303efb7c265371e6ac87fe8bec
Cameron Kolkey <Alleyria@gmail.com>
2019-05-29 18:39:45 +0200

    Bot has an intro sentence before asking first question

--- config/initializers/ruby_bot.rb
+++ config/initializers/ruby_bot.rb
@@ -1,5 +1,5 @@
 SlackRubyBot::Client.logger.level = Logger::INFO
 
 SlackRubyBot.configure do |config|
-  config.logger = Logger.new("slack-ruby-bot.log", "daily")
+  config.logger = Logger.new("log/slack-ruby-bot.log", "daily")
 end
