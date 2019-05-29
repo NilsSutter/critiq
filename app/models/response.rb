4ad96600b8061157ec401ef2ab6cf29ab471078d
Cameron Kolkey <Alleyria@gmail.com>
2019-05-29 17:01:03 +0200

    Multiple choice questions can be sent and replied to properly

--- app/models/response.rb
+++ app/models/response.rb
@@ -3,16 +3,13 @@ class Response < ApplicationRecord
   belongs_to :choice, optional: true
 
   after_commit :send_next_question
-
-
+  
   private
 
   def send_next_question
     puts "** Inside Response Model **"
-    # Find survey associated with this question
-    survey = Question.find(self.question_id).survey
-
     # Filter sent questions for those only pertaining to this user
+    # Return only the last-asked question ID
     last_question_id = SentQuestion.where(recipent_slack_uid: self.slack_uid).last.question_id
     # Sends next question to user:
     SendSlackMessageIndividualJob.perform_later(uid: self.slack_uid, question_id: last_question_id)
