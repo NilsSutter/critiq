f41193776ba499e9e2ae13f290097e1ac8c6cd79
Cameron Kolkey <Alleyria@gmail.com>
2019-06-01 14:39:25 +0200

    Installed Dashkit and upgraded webpack & dependencies

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,8 @@ import "bootstrap";
 import $ from 'jquery';
 import 'select2';                       // globally assign select2 fn to $ element
 import 'select2/dist/css/select2.css';  // optional if you have css loader
-import scrollTo from '../components/smooth_scroll'
+import scrollTo from '../components/smooth_scroll';
+import '../components/dashkit.theme.min';
 
 $(() => {
   $('#survey_channel_id').select2({
@@ -11,9 +12,4 @@ $(() => {
   });
 });
 
-// $( "#slack-channels" ).select2({
-//     theme: "bootstrap"
-// });
-
 scrollTo();
-
