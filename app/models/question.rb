324122559f065c56629b31cdd538cc4199b59f40
Nils Sutter <nilssutter@gmx.de>
2019-06-06 11:31:43 +0200

    check for question.responses and skip chart displaying if responses are empty

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,21 +1,8 @@
 class Question < ApplicationRecord
-  # include PG Search to look up models for database entries
   belongs_to :survey
-  # include PgSearch
-  # pg_search_scope :global_search,
-  #   against: [:name],
-  #   associated_against: {
-  #     survey: [:title, :description]
-  #   },
-  #   using: {
-  #     tsearch: { prefix: true }
-  #   }
-
-  # has_many :responses, through: :choices, -> { where multiple_choice: true }
   has_many :choices, dependent: :destroy
   has_many :sent_questions, dependent: :destroy
   has_many :responses, dependent: :destroy
-
 
   accepts_nested_attributes_for :choices, reject_if: :all_blank, allow_destroy: true
   validates :name, presence: true
